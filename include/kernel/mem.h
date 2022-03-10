4b06c4408fa6754b4ff5734ccee3dccbe9cccd78
LJP-TW <accr94238@gmail.com>
2022-03-09 22:09:32 -0800

    Implement simple allocator

---
+++ include/kernel/mem.h
@@ -0,0 +1,8 @@
+#ifndef _MEM_H
+#define _MEM_H
+
+#include <types.h>
+
+void* simple_malloc(size_t size);
+
+#endif /* _MEM_H */
---
+++ src/kernel/mem.c
@@ -0,0 +1,25 @@
+#include <mem.h>
+#include <mini_uart.h>
+
+char _smem;
+char _emem;
+
+#define SMEM (&_smem)
+#define EMEM (&_emem)
+
+static char *cur = SMEM;
+
+void *simple_malloc(size_t size)
+{
+    char *tmp;
+
+    if ((uint64)cur + size > (uint64)EMEM) {
+        uart_send_string("[!] No enough space!\r\n");
+        return 0;
+    }
+
+    tmp = cur;
+    cur += size;
+
+    return tmp;
+}
