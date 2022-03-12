9c3542fe2362a5def73923d811b85646931d6bf1
LJP-TW <accr94238@gmail.com>
2022-03-12 03:33:49 -0800

    Define some types

--- include/lib/types.h
+++ include/lib/types.h
@@ -6,6 +6,11 @@ typedef unsigned int uint32;
 typedef unsigned short uint16;
 typedef unsigned char uint8;
 
+typedef long long int int64;
+typedef int int32;
+typedef short int16;
+typedef char int8;
+
 typedef uint32 size_t;
 
 #endif /* _TYPES_H */
