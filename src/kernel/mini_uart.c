7d6e592b86413dcc70ea2ab40ce24102f0e3a8c5
LJP-TW <accr94238@gmail.com>
2022-05-16 00:43:17 -0700

    Fix bug in uart_send_num

--- src/kernel/mini_uart.c
+++ src/kernel/mini_uart.c
@@ -158,7 +158,7 @@ static void uart_send_num(sendfp _send_fp, int64 num, int base, int type)
     char tmp[66];
     int i;
 
-    if (type | SIGN) {
+    if (type & SIGN) {
         if (num < 0) {
             (_send_fp)('-');
         }
@@ -170,8 +170,8 @@ static void uart_send_num(sendfp _send_fp, int64 num, int base, int type)
         tmp[i++] = '0';
     } else {
         while (num != 0) {
-            uint8 r = (uint32)num % base;
-            num = (uint32)num / base;
+            uint8 r = (uint64)num % base;
+            num = (uint64)num / base;
             tmp[i++] = digits[r];
         }
     }
